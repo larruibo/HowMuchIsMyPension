758e6d6dc542342d27e2530e35e3da3ba507a928
ar.corinaldi <ar.corinaldi@uniandes.edu.co>
2020-02-14 11:39:02 -0500

    Html en index.html

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
+const testRouter = require('./routes/test');
 
 var app = express();
 
@@ -16,5 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
+// Route para pruebas con node
+app.use('/test', testRouter);
 
 module.exports = app;
