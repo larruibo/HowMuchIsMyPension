499ae04d2f643a1d507694242415a49b43dd0982
ar.corinaldi <ar.corinaldi@uniandes.edu.co>
2020-02-17 21:26:00 -0500

    templating and a new script

---
+++ public/scripts/calculatePension.js
@@ -0,0 +1,9 @@
+const cotizacion = document.getElementById('inputCotizacion');
+const date = document.getElementById('inputDate');
+
+document.getElementById('calculateBtn').onclick = () => {
+  console.log(cotizacion.value, date.value);
+};
+
+
+
