522a4b401d5f6e93cea5cc7dddaa831fc71396be
ar.corinaldi <ar.corinaldi@uniandes.edu.co>
2020-02-25 17:31:44 -0500

    documenting calculatePension.js

--- public/scripts/calculatePension.js
+++ public/scripts/calculatePension.js
@@ -1,19 +1,30 @@
-const darAnioMes = anioMes => anioMes.split("-");
+"use strict";
 
+// Split from "2019-10" to arr = [2019, 10]
+const darAnioMes = anioMes => {
+  let arr = anioMes.split("-");
+  arr[0] = parseInt(arr[0]);
+  arr[1] = parseInt(arr[1]);
+  return arr;
+};
+
+// Render the page, appendChild tr's in a tbody
 const renderPage = (data) => {
+  // tbody element
   let tbody = document.getElementById("content-table");
+  // Iterating over every line of the json file
   data.forEach(element => {
     let anioMes = darAnioMes(element.anio_mes);
-    anioMes[0] = parseInt(anioMes[0]);
-    anioMes[1] = parseInt(anioMes[1]);
+    // tr elment
     let tr = document.createElement("tr");
     tbody.appendChild(tr);
-    console.log(typeof(anioMes[0]));
-    for( let prop in element ){
+    
+    // Iterating through the properties of every json
+    for (let prop in element) {
       let td = document.createElement("td");
       tr.appendChild(td);
-      if( prop !== "anio_mes" ) td.textContent = `${element[prop]}`;
-      else{
+      if (prop !== "anio_mes") td.textContent = `${element[prop]}`;
+      else {
         td.textContent = `${anioMes[0]}`;
         let tdTemp = document.createElement("td");
         tr.appendChild(tdTemp);
@@ -23,6 +34,7 @@ const renderPage = (data) => {
   });
 };
 
+// Getting the data
 fetch("../files/pension.json")
   .then(res => res.json())
   .then(renderPage);
