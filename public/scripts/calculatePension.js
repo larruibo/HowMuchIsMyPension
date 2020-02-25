2729229244e1c8a2b5b9c679c2f3b8a5efcb590d
ar.corinaldi <ar.corinaldi@uniandes.edu.co>
2020-02-25 17:02:31 -0500

    loading ipc table

--- public/scripts/calculatePension.js
+++ public/scripts/calculatePension.js
@@ -1,9 +1,28 @@
-const cotizacion = document.getElementById('inputCotizacion');
-const date = document.getElementById('inputDate');
+const darAnioMes = anioMes => anioMes.split("-");
 
-document.getElementById('calculateBtn').onclick = () => {
-  console.log(cotizacion.value, date.value);
+const renderPage = (data) => {
+  let tbody = document.getElementById("content-table");
+  data.forEach(element => {
+    let anioMes = darAnioMes(element.anio_mes);
+    anioMes[0] = parseInt(anioMes[0]);
+    anioMes[1] = parseInt(anioMes[1]);
+    let tr = document.createElement("tr");
+    tbody.appendChild(tr);
+    console.log(typeof(anioMes[0]));
+    for( let prop in element ){
+      let td = document.createElement("td");
+      tr.appendChild(td);
+      if( prop !== "anio_mes" ) td.textContent = `${element[prop]}`;
+      else{
+        td.textContent = `${anioMes[0]}`;
+        let tdTemp = document.createElement("td");
+        tr.appendChild(tdTemp);
+        tdTemp.textContent = `${anioMes[1]}`;
+      }
+    }
+  });
 };
 
-
-
+fetch("../files/pension.json")
+  .then(res => res.json())
+  .then(renderPage);
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var router = express.Router();
+let pension = require("../modules/pension");
+
 
 /* GET users listing. */
 router.get("/", function(req, res) {
@@ -8,6 +10,17 @@ router.get("/", function(req, res) {
 
 router.get("/tables", function(req, res) {
   res.render("tables");
+});
+
+router.get("/ipc", function(req, res) {
+  res.render("ipc");
+});
+
+router.get("/ipc/data", function(req, res) {
+  let ipc = pension();
+  const file = ipc.fileJson();
+  console.log("this is ipc/data");
+  res.render("base");
 });
 
 module.exports = router;
