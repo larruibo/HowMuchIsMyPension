500b1894c96115498095329b692dd02ed7890d57
Dartm05 <d.arteaga@uniandes.edu.co>
2020-02-29 16:34:21 -0500

    merge

--- app.js
+++ app.js
@@ -23,10 +23,6 @@ const partialsPath = path.join(__dirname, "/templates/partials");
 
 // Setup HBS engine and views locations
 app.set("view engine", "ejs");
-<<<<<<< HEAD
-
-=======
->>>>>>> 7ebc41052618de269306889bad04c73e388aa2d2
 app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
@@ -52,8 +48,6 @@ app.use("/", indexRouter);
 // Ruta de dashboard
 app.use("/dashboard", dashboardRouter);
 
-<<<<<<< HEAD
-=======
 app.post("/login",
   passport.authenticate("local", { failureRedirect: "/login" }),
   function (req, res) {
@@ -64,9 +58,7 @@ app.get("*", (req, res) => {
   res.render("not_found_404");
 });
 
-const LocalStrategy = require("passport-local").Strategy;
 
->>>>>>> 7ebc41052618de269306889bad04c73e388aa2d2
 passport.use(new LocalStrategy(
   function (username, password, done) {
     mongo.findOne({
