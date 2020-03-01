c0ee035c7524050de8b656bd0576e2f09ae7a315
ar.corinaldi <ar.corinaldi@uniandes.edu.co>
2020-03-01 10:48:56 -0500

    documentacion scripts y limpieza de codigo

--- public/scripts/addingInput.js
+++ public/scripts/addingInput.js
@@ -1,22 +1,11 @@
 const butAgregar = document.getElementById("butAgregar");
 const butEliminar = document.getElementById("butEliminar");
-const butEnviar = document.getElementById("butEnviar");
 const butQuitar = document.getElementById("butQuitar");
-
 const divAgregar = document.getElementById("content-input-agregar");
 const divEliminar = document.getElementById("content-input-eliminar");
 
 
-butEnviar.addEventListener("click", () => {
-  // addData();
-  // setTimeout(()=> fetch("/dashboard/agregarCot"), 1000 );
-});
-
-butQuitar.addEventListener("click", () => {
-  let input = document.getElementById("iSemanaCotizada");
-  eliminarData(input);
-});
-
+// Muestra la parte de eliminar 
 butEliminar.addEventListener("click", () => {
   butEliminar.classList.toggle("true");
 
@@ -42,8 +31,7 @@ butEliminar.addEventListener("click", () => {
   }
 });
 
-
-
+// Muestra la parte la parte de agregar
 butAgregar.addEventListener("click", () => {
   butAgregar.classList.toggle("true");
 
@@ -57,74 +45,5 @@ butAgregar.addEventListener("click", () => {
     butAgregar.setAttribute("add", "true");
     butEliminar.removeAttribute("disabled");
   }
-
 });
 
-function addData() {
-
-  let tbody = document.getElementById("table-content");
-  let tr = document.createElement("tr");
-  tbody.appendChild(tr);
-
-  let tds = [
-    document.createElement("td"),
-    document.createElement("td"),
-    document.createElement("td"),
-    document.createElement("td"),
-    document.createElement("td"),
-  ];
-
-  //tds[0] = cotizacion
-  tds[0].textContent = parseInt(document.getElementById("iCotizacion").value);
-
-  let anio_mes = document.getElementById("iMesAnio").value;
-  const arrAnioMes = darAnioMes(anio_mes);
-  // Anio
-  tds[1].textContent = arrAnioMes[0];
-  // Mes
-  tds[2].textContent = arrAnioMes[1];
-
-
-  fetch("../files/pension.json")
-    .then(res => res.json())
-    .then((data) => {
-      let ipc = data[2020 - arrAnioMes[0] + arrAnioMes[1] - 1].inflacion_mensual;
-      // IPC cargado desde el archivo pension.json
-      tds[3].textContent = ipc;
-    });
-
-  let tdList = document.querySelectorAll("tbody > tr");
-  tds[4].textContent = tdList.length;
-  tr.setAttribute("id", tdList.length + "");
-
-  tds.forEach((td) => {
-    tr.appendChild(td);
-  });
-}
-
-// Split from "2019-10" to arr = [2019, 10]
-const darAnioMes = anioMes => {
-  let arr = anioMes.split("-");
-  arr[0] = parseInt(arr[0]);
-  arr[1] = parseInt(arr[1]);
-  return arr;
-};
-
-function eliminarData(input) {
-  let tr = document.getElementById(input.value);
-  let trs = document.querySelectorAll("tbody > tr");
-
-  let iSemanaCotizada = document.getElementById("iSemanaCotizada");
-  if (trs.length-1 === 0) {
-    iSemanaCotizada.setAttribute("disabled", "disabled");
-    butQuitar.setAttribute("disabled", "disabled");
-  }
-  else {
-    iSemanaCotizada.removeAttribute("disabled");
-    butQuitar.removeAttribute("disabled");
-  }
-
-  if (input.value <= trs.length && input.value >= 0) {
-    tr.remove();
-  }
-}
