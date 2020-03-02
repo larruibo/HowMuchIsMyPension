1f47669df370a8cca6aff83dad7284c87d9e77e2
ar.corinaldi <ar.corinaldi@uniandes.edu.co>
2020-03-02 10:34:13 -0500

    piing

--- routes/index.js
+++ routes/index.js
@@ -51,7 +51,7 @@ passport.use("local-signup", new LocalStrategy({
 
   // find a user whose email is the same as the forms email
   // we are checking to see if the user trying to login already exists
-  mongo.passport.findOne({ username: username }), function (err, username) {
+  mongo.passport.findOne({ username: username }, function (err, username) {
     // if there are any errors, return the error
     if (err)
       return done(err);
